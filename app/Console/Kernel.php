e62deadbaac994b3356a685142c4dee457477803
tetrakiss <togulev@gmail.com>
2022-12-20 18:22:55 +0300

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call('App\Http\Controllers\amoLeads@refreshToken')
+                ->hourly();
     }
 
     /**
